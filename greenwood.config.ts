4515cecd28b751985fe71a91bbd5cc18d58fd72d
Owen Buckley <owenbuckley13@gmail.com>
2025-10-05 19:53:55 -0400

    feat: masonry grid layout w/ tailwind (#3)

--- greenwood.config.ts
+++ greenwood.config.ts
@@ -1,8 +1,12 @@
+import { greenwoodPluginPostCss } from "@greenwood/plugin-postcss";
 import type { Config } from "@greenwood/cli";
 
 // https://greenwoodjs.dev/docs/reference/configuration/
 const config: Config = {
   prerender: true,
+  plugins: [
+    greenwoodPluginPostCss(),
+  ]
 };
 
 export default config;
