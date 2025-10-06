8b03f13913d33a62f1fa520496438e50fff65367
Owen Buckley <owenbuckley13@gmail.com>
2025-10-05 20:55:18 -0400

    misc tweaks

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -2,7 +2,7 @@ export default class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <footer class="text-center italic mb-4">
-        <span>Last update: ${new Date().toISOString()}</span>
+        <span>Last updated: ${new Date().toISOString()}</span>
       </footer>
     `
   }
